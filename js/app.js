84273be008c5aaba6f6d00249bad6e7c80138d56
irhaminsanmakarim <irhamkarim@gmail.com>
2015-07-01 10:34:22 +0700

    add collapse menu

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$('#collapse-menu').click(function(){
+  $('html').toggleClass('menu-collapse');
+});
